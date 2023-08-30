24f52d3d7560c7c4b2c468c6f27362cb32bb14a1
r4nd0lph <r11345roma@yandex.ru>
2023-08-30 15:30:56 +0300

    added TSP.__draw_paths()

--- algorithms/TSP.py
+++ algorithms/TSP.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from random import randint
 from numpy import array
 from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 
 
 def generate_problem(count: int, canvas_size: int = 1000) -> list[tuple[int]]:
@@ -30,6 +31,15 @@ class TSP:
     """
 
     CLR_POINT = "#eb343a"
+    CLR_PATH = [
+        "#eb343a",
+        "#db34eb",
+        "#5b34eb",
+        "#34b4eb",
+        "#34eb4c",
+        "#ebe534",
+        "#eb9234",
+    ]
 
     def __init__(self, points: list[tuple[int]], paths: list[Path] = None) -> None:
         """..."""
@@ -77,10 +87,28 @@ class TSP:
                 fontsize=6,
             )
 
+    def __draw_paths(self) -> list[Line2D]:
+        """..."""
+
+        lines = []
+        if self.__paths:
+            for i, path in enumerate(self.__paths):
+                points = [self.__points[i] for i in path.indx]
+                (l,) = plt.plot(
+                    *array(points).T,
+                    ls="--",
+                    zorder=0,
+                    color=TSP.CLR_PATH[i % len(TSP.CLR_PATH)],
+                    label=f"{path.name} ({path.leng:.2f})",
+                )
+                lines.append(l)
+        return lines
+
     def __show(self) -> None:
         """..."""
 
         self.__draw_points()
+        self.__draw_paths()
         plt.show()
 
 
--- algorithms/usage.py
+++ algorithms/usage.py
@@ -1,11 +1,15 @@
-from TSP import generate_problem, TSP
+from TSP import generate_problem, Path, TSP
 
 
 def main() -> None:
     """..."""
 
-    points = generate_problem(20)
-    tsp = TSP(points=points, paths=None)
+    points = generate_problem(5)
+    paths = [
+        Path(indx=[0, 1, 2, 3, 4, 0], leng=128.0, name="Path #1"),
+        Path(indx=[0, 2, 4, 1, 3, 0], leng=256.0, name="Path #2"),
+    ]
+    tsp = TSP(points=points, paths=paths)
 
 
 if __name__ == "__main__":
