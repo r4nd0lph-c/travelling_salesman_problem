cd2e8aa4851e2cf2c4f5591bac83b7ce488a7def
r4nd0lph <r11345roma@yandex.ru>
2023-08-30 14:49:57 +0300

    init repo

---
+++ algorithms/TSP.py
@@ -0,0 +1,16 @@
+# Travelling Salesman Problem
+
+
+class TSP:
+    """
+    ...
+    """
+
+    def __init__(self) -> None:
+        """..."""
+
+        pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ algorithms/usage.py
@@ -0,0 +1,11 @@
+from TSP import TSP
+
+
+def main() -> None:
+    """..."""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
